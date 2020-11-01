9008e90dcef22845ea8f14d57c5b38658c292989
hzx <694941728@qq.com>
2020-11-02 00:39:02 +0800

    a

--- peak_find_lstm/wirehouse/generate/findline.m
+++
@@ -1,9 +0,0 @@
-clc;clear all;
-load pure.mat;
-x=data.data_x;
-figure;plot(x);
-n_x=length(x);
-f_x=[1:n_x];
-% [x0,y0]=invinterp(f_x,x,0.5);
-[x0,y0]=invinterp(f_x(520:540),x(520:540),0.5*max(x(520:540)));
-lw_x=abs(x0(1)-x0(2))*2000/2048;
